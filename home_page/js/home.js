82ec9951a0ac89d1bd7221091ba35a17aee5a0a6
hagar-gamal <hagar28gamal@gmail.com>
2025-05-10 03:41:53 +0300

    save user data in all data when logout

--- home_page/js/home.js
+++ home_page/js/home.js
@@ -1,3 +1,18 @@
+
+
+let loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+let allData = JSON.parse(localStorage.getItem("all_data"));
+
 document.getElementById("profileIcon").addEventListener("click", () => {
-    window.location.href = "../../login/login.html";
+    if (loggedInUser) {
+        let userIndex = allData.users.findIndex(user => user.id == loggedInUser.id);
+        if (userIndex !== -1) {
+            allData.users[userIndex] = loggedInUser;
+            localStorage.setItem("all_data", JSON.stringify(allData));
+        }
+
+        // --- go to profile page here
+    } else {
+        window.location.href = "../../login/login.html";
+    }
 });
