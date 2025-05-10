971d2a3d06c931655db565f40d7170a33b92df65
Alaa Elnady <alaahamdialnadi@gmail.com>
2025-05-10 18:50:38 +0200

    Adding Search Functionality and updating some styles in header

--- home_page/js/home.js
+++ home_page/js/home.js
@@ -1,3 +1,22 @@
 document.getElementById("profileIcon").addEventListener("click", () => {
-    window.location.href = "../../login/login.html";
+  window.location.href = "../../login/login.html";
+});
+
+let data = JSON.parse(localStorage.getItem("all_data"));
+let filtered = [];
+
+let searchInput = document.getElementById("prosSearch");
+searchInput.addEventListener("change", () => {
+  let searchValue = searchInput.value.toLowerCase().trim();
+
+  filtered = data.products.filter((product) =>
+    product.name.toLowerCase().includes(searchValue)
+  );
+
+  if (!filtered.length) {
+    // allPros.innerHTML = `<p style="grid-column: 1 / -1; text-align: center; font-size: 1.2rem; color: #666;">No result match your search.</p>`;
+    alert("No result match your search."); // optional: format or render as needed
+  } else {
+    alert(JSON.stringify(filtered)); // optional: format or render as needed
+  }
 });
